a427a3ff6f050bde604f9979e4d8fcaf81fe514a
jhoysi93 <jhoysi939@gmail.com>
2017-10-25 18:28:00 -0400

    Preparano el componente Test, ya muestra pero falta vaidar las respuestas de los radio buttons

--- src/app/components/contributions/exam/exam.component.ts
+++ src/app/components/contributions/exam/exam.component.ts
@@ -1,3 +1,5 @@
+import { forkJoin } from 'rxjs/observable/forkJoin';
+import { forEach } from '@angular/router/src/utils/collection';
 import { Component, OnInit } from '@angular/core';
 import { ContributionsService } from '../../../services/contributions.service';
 
@@ -8,21 +10,85 @@ import { ContributionsService } from '../../../services/contributions.service';
 })
 export class ExamComponent implements OnInit {
 
-  public exam:any;
-  public titleText:string="TEST";
-  public aprobaste:boolean= false;
-  
-  constructor( public _contributionsService: ContributionsService) { 
-    console.log(_contributionsService.tests);
+  public exam: any;
+  public titleText: string = "TEST";
+  public aprobaste: boolean = false;
+  public showTest: boolean = false;
+  public listOptions: any[];
+  public listOptionsTotal: any[] =[];
+  public listTitleQuestion: string[] = [];
+  public titulo:string = "";
+
+
+  constructor(public _contributionsService: ContributionsService) {
   }
 
   ngOnInit() {
-  }
-  public loadExam(test:string){
-    this.titleText = test;
-  }
-  /* loadExam(indice:number){
+    /*if (this._contributionsService.tests == undefined) {
 
-  } */
-  
+      this._contributionsService.getTests();
+      this.listTest = this._contributionsService.tests;
+
+    }*/
+  }
+  public loadExam(index: number) {
+    this._contributionsService.getTest(index);
+    this._contributionsService.getTests();
+    this.titleText = "CARGANDO...";
+    this.listTitleQuestion = [];
+    this.listOptionsTotal= [];
+    this.listOptions = [];
+    this.showTest =  false;
+
+    setTimeout(() => {
+
+      for (let ind = 0; ind < 5; ind++) {
+
+        //Obtenemos las respuestas de las opciones de cada pregunta
+        this.listOptions = this._contributionsService.tests[index].questions[ind].answers.options;
+        this.listOptionsTotal.push(this.listOptions);
+        this.listOptions = [];
+
+        //obtenemos el titulo de cada pregunta
+        this.titulo = this._contributionsService.test.questions[ind].title;
+        this.listTitleQuestion.push(this.titulo);
+        this.titulo = "";
+      }
+
+      console.log(this.listTitleQuestion);
+      console.log(this.listOptionsTotal);
+      this.titleText = this._contributionsService.test.title;
+      this.showTest = true;
+
+
+    }, 1000);
+
+
+
+  }
+
+
+  ///////Dede aqui estoy intentando  tampoco da mandando el valor de la linea 529 en el html donde esta el clicks
+  public checkAnswers(value: any) {
+    
+    ///este codigo encontre pero no da
+    
+    let resultado:any ="ninguno";
+    
+    let valorRadioButon:any = document.getElementsByName("radio-stacked1");
+    console.log(valorRadioButon);
+    // Recorremos todos los valores del radio button para encontrar el
+           // seleccionado
+           for(var i=0;i<valorRadioButon.length;i++)
+           {
+               if(valorRadioButon[i].checkAnswers){
+                console.log(valorRadioButon[i]);
+                 resultado=valorRadioButon[i].value;
+               }
+           }
+
+    console.log(resultado);
+    
+  }
+
 }
--- src/app/services/contributions.service.ts
+++ src/app/services/contributions.service.ts
@@ -4,54 +4,67 @@ import { Http } from '@angular/http';
 @Injectable()
 export class ContributionsService {
 
-    public themes:any[];
-    public books:any[];
-    public book:any;
-    public tests:any[];
+  public themes: any[];
+  public books: any[];
+  public book: any;
+  public tests: any[];
+  public test:any;
 
-    constructor(private http:Http) { 
-        this.getThemes();
-        this.getTests();
-    }
+  constructor(private http: Http) {
 
-    public getThemes(){
-        this.http.get("https://redesneuroalesweb.firebaseio.com/themes.json").subscribe( 
-          (data) =>{
-            this.themes = data.json();
-            console.log(data.json());
-          }
-        );
+    this.getThemes();
+
+  }
+
+  public getThemes() {
+    this.http.get("https://redesneuroalesweb.firebaseio.com/themes.json").subscribe(
+      (data) => {
+        this.themes = data.json();
+        console.log(data.json());
       }
+    );
+  }
 
 
-      public getBooks(){
-        this.http.get("https://redesneuroalesweb.firebaseio.com/books.json").subscribe( 
-          (data) =>{
-            this.books = data.json();
-            console.log("data.Books");
-            console.log(data.json());
-          }
-        );
+  public getBooks() {
+    this.http.get("https://redesneuroalesweb.firebaseio.com/books.json").subscribe(
+      (data) => {
+        this.books = data.json();
+        console.log("data.Books");
+        console.log(data.json());
       }
-      
-      public getBook(idBook:number){
-        
-        this.http.get(`https://redesneuroalesweb.firebaseio.com/books/${idBook}.json`).subscribe( 
-          (data) =>{
-            this.book = data.json();
-            console.log("data.Book");
-            console.log(data.json());
-          }
-        );
-      }
+    );
+  }
 
-      public getTests(){
-        this.http.get("https://redesneuroalesweb.firebaseio.com/exams.json").subscribe(
-            (data)=>{
-                this.tests = data.json();
-                console.log("data tests");
-                console.log(data.json());
-            }
-        )
-    }
+  public getBook(idBook: number) {
+
+    this.http.get(`https://redesneuroalesweb.firebaseio.com/books/${idBook}.json`).subscribe(
+      (data) => {
+        this.book = data.json();
+        console.log("data.Book");
+        console.log(data.json());
+      }
+    );
+  }
+
+  public getTests() {
+    this.http.get("https://redesneuroalesweb.firebaseio.com/exams.json").subscribe(
+      (data) => {
+        this.tests = data.json();
+        console.log("data tests");
+        console.log(this.tests);
+      }
+    );
+  }
+
+
+  public getTest(idTest: number) {
+    this.http.get(`https://redesneuroalesweb.firebaseio.com/exams/${idTest}.json`).subscribe(
+      (data) =>{
+        this.test = data.json();
+        console.log("data test unico");
+        console.log(this.test);
+      }
+    );
+  }
 }
